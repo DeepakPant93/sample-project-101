5eda73abeb66265e8639480aa8724c0be5aae7f1
deepakpant <deepak.93p@gmail.com>
2024-12-16 22:55:33 +0530

    Bump version: 0.0.3 → 0.0.4

--- sample_project_101/__init__.py
+++ sample_project_101/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 """
 This project is a Data Science application built with FastAPI, designed to facilitate model training, prediction, and data processing. The application uses Poetry for dependency management and follows modular design principles to ensure scalability and maintainability.
--- sample_project_101/main.py
+++ sample_project_101/main.py
@@ -4,7 +4,7 @@ from fastapi_health import health
 
 from sample_project_101.routes.router import router
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 app = FastAPI(
     title="sample-project-101 APIs",
